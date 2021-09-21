8f81bf7fad0e0fdbbfe140a849cf90d85581185d
Rahul Kumar <rahulpathak263@gmail.com>
2021-09-21 14:38:18 +0530

    Update run.py

--- app/run.py
+++ app/run.py
@@ -1,16 +1,12 @@
 import json
-import plotly
-import pandas as pd
-import numpy as np
+
 import joblib
-
-from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
-
+import pandas as pd
+import plotly
 from flask import Flask
 from flask import render_template, request
-from plotly.graph_objs import Bar
-
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
 from sqlalchemy import create_engine
 
 app = Flask(__name__)
@@ -41,74 +37,66 @@ model = joblib.load("../models/classifier.pkl")
 @app.route('/index')
 def index():
     # extract data needed for visuals
-    genre_counts = df.groupby('genre').count()['message']
-    genre_names = list(genre_counts.index)
+    # TODO: Below is an example - modify to extract data for your own visuals
+    gen_count = df.groupby('genre').count()['message']
+    gen_per = round(100 * gen_count / gen_count.sum(), 2)
+    gen = list(gen_count.index)
+    cat_num = df.drop(['id', 'message', 'original', 'genre'], axis=1).sum()
+    cat_num = cat_num.sort_values(ascending=False)
+    cat = list(cat_num.index)
 
-    # Show distribution of different category
-    category = list(df.columns[4:])
-    category_counts = []
-    for column_name in category:
-        category_counts.append(np.sum(df[column_name]))
-
-    # extract data exclude related
-    categories = df.iloc[:, 4:]
-    categories_mean = categories.mean().sort_values(ascending=False)[1:11]
-    categories_names = list(categories_mean.index)
+    colors = ['yellow', 'green', 'red']
 
     # create visuals
     graphs = [
         {
-            'data': [
-                Bar(
-                    x=genre_names,
-                    y=genre_counts
-                )
+            "data": [
+                {
+                    "type": "pie",
+                    "uid": "f4de1f",
+                    "hole": 0.4,
+                    "name": "Genre",
+                    "pull": 0,
+                    "domain": {
+                        "x": gen_per,
+                        "y": gen
+                    },
+                    "marker": {
+                        "colors": [
+                            "#7fc97f",
+                            "#beaed4",
+                            "#fdc086"
+                        ]
+                    },
+                    "textinfo": "label+value",
+                    "hoverinfo": "all",
+                    "labels": gen,
+                    "values": gen_count
+                }
             ],
-
-            'layout': {
-                'title': 'Distribution of Message Genres',
+            "layout": {
+                "title": "Count and Percent of Messages by Genre"
+            }
+        },
+        {
+            "data": [
+                {
+                    "type": "bar",
+                    "x": cat,
+                    "y": cat_num,
+                    "marker": {
+                        "color": 'grey'}
+                }
+            ],
+            "layout": {
+                "title": "Count of Messages by Category",
                 'yaxis': {
                     'title': "Count"
                 },
                 'xaxis': {
                     'title': "Genre"
-                }
-            }
-        },
-        {
-            'data': [
-                Bar(
-                    x=category,
-                    y=category_counts
-                )
-            ],
-
-            'layout': {
-                'title': 'Distribution of Message Categories',
-                'yaxis': {
-                    'title': "Count"
                 },
-                'axis': {
-                    'title': "Category"
-                }
-            }
-        },
-        {
-            'data': [
-                Bar(
-                    x=categories_names,
-                    y=categories_mean
-                )
-            ],
-
-            'layout': {
-                'title': 'Top 10 Message Categories',
-                'yaxis': {
-                    'title': "Percentage"
-                },
-                'axis': {
-                    'title': "Categories"
-                }
+                'barmode': 'group'
             }
         }
     ]
@@ -119,7 +107,6 @@ def index():
 
     # render web page with plotly graphs
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
-
 
 # web page that handles user query and displays model results
 @app.route('/go')
